c4c9864cc87268114b0c03359df18eccd11d1e53
Lyasinkovska <l.yasinkovska@gmail.com>
2020-02-26 13:02:40 +0200

    add espresso, latte, cappuccino functions, while loop

--- Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/Coffee Machine/task/machine/coffee_machine.py
@@ -4,10 +4,6 @@
       'beans\nPouring coffee into the cup\nPouring some '
       'milk into the cup\nCoffee is ready!')
 
-water = 200
-milk = 50
-beans = 15
-
 result = [water_c/water, milk_c/milk, beans_c/beans]
 min_res = int((min(result)))
 
@@ -24,45 +20,57 @@ coffee = {"machine": {"water": 1200, "milk": 540, "beans": 120, "cups": 9, "mone
           "latte": {"water": 350, "milk": 75, "beans": 20, "cups": 1, "price": 7},
           "cappuccino": {"water": 200, "milk": 100, "beans": 12, "cups": 1, "price": 6}}
 
-water = coffee["machine"]["water"]
-milk = coffee["machine"]["milk"]
-beans = coffee["machine"]["beans"]
-cups = coffee["machine"]["cups"]
-money = coffee["machine"]["money"]
 
 
-def machine_state(water = water, milk = milk, beans = beans, cups=cups, money = money):
+
+def change_filling():
+    pass
+
+def remaining(water, milk, beans, cups, money):
+    #change_filling(water, milk, beans, cups, money)
     return "The coffee machine has:\n{} of water\n{} of milk\n{} of coffee beans" \
                 "\n{} of disposable cups\n{} of money".format(water, milk, beans, cups, money)
 
 
-def buy(water, milk, beans,cups, money):
+def espresso(water, milk, beans, cups, money):
+    coffee["machine"]["water"] -= coffee["espresso"]["water"]
+    coffee["machine"]["beans"] -= coffee["espresso"]["beans"]
+    coffee["machine"]["milk"] -= coffee["espresso"]["milk"]
+    coffee["machine"]["cups"] -= coffee["espresso"]["cups"]
+    coffee["machine"]["money"] += coffee["espresso"]["price"]
+    return water, beans, milk, cups, money
+
+
+def latte(water, milk, beans, cups, money):
+    coffee["machine"]["water"] -= coffee["latte"]["water"]
+    coffee["machine"]["beans"] -= coffee["latte"]["beans"]
+    coffee["machine"]["milk"] -= coffee["latte"]["milk"]
+    coffee["machine"]["cups"] -= coffee["latte"]["cups"]
+    coffee["machine"]["money"] += coffee["latte"]["price"]
+    return water, beans, milk, cups, money
+
+
+def cappuccino(water, milk, beans, cups, money):
+    coffee["machine"]["water"] -= coffee["cappuccino"]["water"]
+    coffee["machine"]["beans"] -= coffee["cappuccino"]["beans"]
+    coffee["machine"]["milk"] -= coffee["cappuccino"]["milk"]
+    coffee["machine"]["cups"] -= coffee["cappuccino"]["cups"]
+    coffee["machine"]["money"] += coffee["cappuccino"]["price"]
+    return water, beans, milk, cups, money
+
+
+def buy(water, milk, beans, cups, money):
     buy = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n")
     print(buy)
     if buy == "1":
-        water -= coffee["espresso"]["water"]
-        beans -= coffee["espresso"]["beans"]
-        milk -= coffee["espresso"]["milk"]
-        cups -= coffee["espresso"]["cups"]
-        money += coffee["espresso"]["price"]
-        print(machine_state(water, milk, beans, cups, money))
+        espresso(water, milk, beans,cups, money)
     elif buy == "2":
-        water -= coffee["latte"]["water"]
-        beans -= coffee["latte"]["beans"]
-        milk -= coffee["latte"]["milk"]
-        cups -= coffee["latte"]["cups"]
-        money += coffee["latte"]["price"]
-        print(machine_state(water, milk, beans, cups, money))
+        latte(water, milk, beans,cups, money)
     elif buy == "3":
-        water -= coffee["cappuccino"]["water"]
-        beans -= coffee["cappuccino"]["beans"]
-        milk -= coffee["cappuccino"]["milk"]
-        cups -= coffee["cappuccino"]["cups"]
-        money += coffee["cappuccino"]["price"]
-        print(machine_state(water, milk, beans, cups, money))
+        cappuccino(water, milk, beans,cups, money)
 
 
-def fill(water, milk, beans, cups, money = money):
+def fill(water, milk, beans,cups):
     water_fill= int(input("Write how many ml of water do you want to add:\n"))
     milk_fill = int(input("Write how many ml of milk do you want to add:\n"))
     beans_fill = int(input("Write how many grams of coffee beans do you want to add:\n"))
@@ -71,20 +79,39 @@ def fill(water, milk, beans, cups, money = money):
     milk += milk_fill
     beans += beans_fill
     cups += cups_fill
-    print(machine_state(water, milk, beans, cups, money))
+
 
 def take(water, milk, beans, cups, money):
     print("I gave you ${}".format(money))
     money -= money
-    print(machine_state(water, milk, beans, cups, money))
 
-print(machine_state())
 
-action = input("Write action (buy, fill, take):\n")
+def back():
+    pass
 
-if action == "buy":
-    buy(water, milk, beans, cups, money)
-elif action == "fill":
-    fill(water, milk, beans, cups, money)
-elif action == "take":
-    take(water, milk, beans, cups, money)
+
+while True:
+    water = coffee["machine"]["water"]
+    milk = coffee["machine"]["milk"]
+    beans = coffee["machine"]["beans"]
+    cups = coffee["machine"]["cups"]
+    money = coffee["machine"]["money"]
+
+    action = input("Write action (buy, fill, take, remaining, exit):\n")
+
+    if action == "buy":
+        buy(water, milk, beans, cups, money)
+        continue
+    elif action == "fill":
+        fill(water, milk, beans, cups)
+        continue
+    elif action == "take":
+        take(water, milk, beans, cups, money)
+        continue
+    elif action == "remaining":
+        print(remaining(water, milk, beans, cups, money))
+        continue
+    elif action == "back":
+        back()
+    elif action == "exit":
+        break
