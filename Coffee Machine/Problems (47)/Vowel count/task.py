7816051fc92cc4ebee27c3b90cdd4d84476a755b
Lyasinkovska <l.yasinkovska@gmail.com>
2020-02-26 11:48:42 +0200

    new tasks added

---
+++ Coffee Machine/Problems (47)/Vowel count/task.py
@@ -0,0 +1,9 @@
+string = "red yellow fox bite orange goose beeeeeeeeeeep"
+vowels = 'aeiouy'
+count = 0
+
+# fix this for loop
+for i in string:
+    if i in vowels:
+        count += 1
+#print(count)
