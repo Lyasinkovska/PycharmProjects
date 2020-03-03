adca1ae092d397e758557797e49e74c76ee3fae1
Lyasinkovska <l.yasinkovska@gmail.com>
2020-03-03 12:42:56 +0200

    task Stack updated

--- Coffee Machine/Problems (69)/Stack class/task.py
+++ Coffee Machine/Problems (69)/Stack class/task.py
@@ -5,28 +5,12 @@ class Stack:
 
     def push(self, el):
         self.stack.append(el)
-        return self.stack
 
     def pop(self):
-        self.stack.pop()
-        return self.stack
+        return self.stack.pop()
 
     def peek(self):
         return self.stack[-1]
 
     def is_empty(self):
         return len(self.stack) < 1
-
-
-
-'''stack = Stack()
-stack.push("first")
-print(stack.stack)
-stack.push("second")
-print(stack.stack)
-stack.peek()
-print([stack.peek()])
-#print(stack.stack)
-stack.pop()
-print(stack.stack)
-print(stack.is_empty())'''
