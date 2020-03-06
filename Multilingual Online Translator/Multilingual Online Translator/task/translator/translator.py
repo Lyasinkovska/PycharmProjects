cee73c3d8e09da114741a63e3a2721b95049f620
Lyasinkovska <l.yasinkovska@gmail.com>
2020-03-06 16:24:46 +0200

    new

--- Multilingual Online Translator/Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/Multilingual Online Translator/task/translator/translator.py
@@ -1,1 +1,12 @@
-print('Hello, World!')
+def translator():
+	choosing_language = input(f"Type \"en\" if you want to translate from French into English, "
+							  f"or \"fr\" if you want to translate from English into French:\n")
+
+	word_selection = input("Type the word you want to translate:\n")
+	result = f"You chose \"{choosing_language}\" as a language to translate \"{word_selection}\""
+	print(choosing_language)
+	print(word_selection)
+	print(result)
+
+
+translator()
