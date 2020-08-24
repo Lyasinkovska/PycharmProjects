93c5b15ec70e0036010bfff3503898fa1e682309
Lyasinkovska <l.yasinkovska@gmail.com>
2020-08-24 15:47:02 +0300

    deleted add_credentials method, deleted credentials dictionary

--- Simple Banking System/Simple Banking System/task/banking/banking.py
+++ Simple Banking System/Simple Banking System/task/banking/banking.py
@@ -1,33 +1,39 @@
 import random
 import sqlite3
 
-import self as self
-
 
 class CardDataBase:
 
     def __init__(self):
-        self.connection = sqlite3.connect ("card.s3db")
+        self.connection = sqlite3.connect("card.db")
         self.cursor = self.connection.cursor()
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS card
+                                                    (
+                                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                    number TEXT,
+                                                    pin TEXT,
+                                                    balance INTEGER DEFAULT 0
+                                                    )"""
+                            )
 
     def cursor_creation(self, card_number, pin):
         self.data_insertion(self.cursor, card_number, pin)
+
         self.connection.commit()
-        #connection.close()
+# connection.close()
 
-    def data_insertion(self, cursor, card_number, pin):
+    @staticmethod
+    def data_insertion(cursor, card_number, pin):
+        cursor.execute("""INSERT INTO card ( number, pin, balance)  VALUES (?,?,?);""", (str(card_number), str(pin), 0))
 
-        cursor.execute("""CREATE TABLE IF NOT EXISTS card
-                                            (
-                                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                            number TEXT,
-                                            pin TEXT,
-                                            balance INTEGER DEFAULT 0
-                                            )"""
-        )
-
-        cursor.execute("""INSERT INTO card ( number, pin, balance)  VALUES (?,?,?);""", (str(card_number), (pin), 0))
-
+    @staticmethod
+    def select_data(cursor, input_card_number, input_pin):
+        cursor.execute("SELECT number, pin FROM card WHERE number = '{}' AND pin = '{}'".format(input_card_number,
+                                                                                                input_pin))
+        if cursor.fetchone() is None:
+            return False
+        else:
+            return True
 
 
 class Account:
@@ -35,17 +41,17 @@ class Account:
     def __init__(self):
         self.first_level_menu = "1. Create an account\n2. Log into account\n0. Exit\n"
         self.second_level_menu = "1. Balance\n2. Log out\n0. Exit\n"
-        self.credentials = dict()
+        #self.credentials = dict()
         self.card_db = CardDataBase()
         self.choice()
-
 
     @staticmethod
     def user_choice(action):
         user_input = input(action)
         return user_input.isnumeric() and int(user_input)
 
-    def luhn_algorithm(self, init_card_number):
+    @staticmethod
+    def luhn_algorithm(init_card_number):
         luhn_card_1 = [int(digit) * 2 for digit in init_card_number[::2]] + [int(digit) for digit in
                                                                              init_card_number[1::2]]
         luhn_card_2 = [(digit - 9) if digit > 9 else digit for digit in luhn_card_1]
@@ -69,19 +75,17 @@ class Account:
     def account_creation(self):
         card_number = self.card_number_generation()
         pin = self.pin_generation()
-        self.add_credentials(card_number, pin)
+        #self.add_credentials(card_number, pin)
         self.card_db.cursor_creation(card_number, pin)
-        print("Your card has been created\nYour card number:\n{}\nYour card PIN:\n{}" \
-              .format(card_number, pin))
+        print("Your card has been created\nYour card number:\n{}\nYour card PIN:\n{}".format(card_number, pin))
 
-    def add_credentials(self, card_number, pin):
-        self.credentials[card_number] = pin
+    #def add_credentials(self, card_number, pin):
+    #    self.credentials[card_number] = pin
 
     def log_in(self):
         input_card_number = self.user_choice('Enter your card number:\n')
         input_pin = self.user_choice("Enter your PIN:\n")
         self.verification(input_card_number, input_pin)
-
 
     @staticmethod
     def balance(balance):
@@ -93,7 +97,8 @@ class Account:
         quit()
 
     def verification(self, input_card_number, input_pin):
-        if input_card_number in self.credentials and self.credentials[input_card_number] == input_pin:
+        if self.card_db.select_data(self.card_db.cursor, input_card_number, input_pin):
+        #if input_card_number in self.credentials and self.credentials[input_card_number] == input_pin:
             print("You have successfully logged in!\n")
             while True:
                 action = self.user_choice(self.second_level_menu)
@@ -124,3 +129,4 @@ class Account:
 
 
 my_account = Account()
+
--- Simple Banking System/Simple Banking System/task/banking/database_draft.py
+++ Simple Banking System/Simple Banking System/task/banking/database_draft.py
@@ -3,18 +3,18 @@ import sqlite3
 connection = sqlite3.connect("draft.db")
 
 cursor = connection.cursor()
-
-'''cursor.execute("""CREATE TABLE family(
+cursor.execute("""CREATE TABLE IF NOT EXISTS family(
 				name VARCHAR(20),
 				surname VARCHAR(20),
 				age INTEGER,
 				sex TEXT
 				)
 				"""
-)'''
-cursor.execute("""INSERT INTO family VALUES ('Liudmyla', 'Yasinkovska', 29, 'female')""")
+)
+#cursor.execute("""INSERT INTO family VALUES ('Liudmyla', 'Yasinkovska', 29, 'female')""")
 cursor.execute("""INSERT INTO family VALUES ("Yura", "Mamonov", 37, "male")""")
-cursor.execute("""SELECT * FROM family""")
+cursor.execute("SELECT name, surname FROM family WHERE name = 'Yura' AND surname = 'Mamonov'")
+print(cursor.fetchone())
 print(cursor.fetchall())
 
 connection.commit()
