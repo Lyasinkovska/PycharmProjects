f7fe5497e6402bb2d4ab73276196913cf840d1f2
Lyasinkovska <l.yasinkovska@gmail.com>
2020-12-21 13:49:42 +0200

    added try_except block to 4 task with asterisk of lesson 3

--- Beetroot/lesson_3/task_asterisk.py
+++ Beetroot/lesson_3/task_asterisk.py
@@ -62,14 +62,18 @@ start = int(input("Please, enter start number:\n"))
 stop = int(input("Please, enter stop number:\n"))
 number = random.randint(start, stop)
 while True:
-	user_number = int(input(f"Try to guess number in the range ({start}, {stop})\n"))
-	if user_number < start or user_number > stop:
-		print("You are out of range")
-		continue
-	elif user_number == number:
-		print("Excellent. You are the winner!")
-		break
-	elif number > user_number:
-		print(f"The number is greater than {user_number}")
-	elif number < user_number:
-		print(f"The number is less than {user_number}")
+	try:
+		user_number = int(input(f"Try to guess number in the range ({start}, {stop})\n"))
+	except ValueError:
+		print("Your guess must be a number.")
+	else:
+		if user_number < start or user_number > stop:
+			print("You are out of range")
+			continue
+		elif user_number == number:
+			print("Excellent. You are the winner!")
+			break
+		elif number > user_number:
+			print(f"The number is greater than {user_number}")
+		elif number < user_number:
+			print(f"The number is less than {user_number}")
