1a151552920f00b4b6a989513c2f89e02f0f0aa1
Lyasinkovska <l.yasinkovska@gmail.com>
2020-12-28 15:01:03 +0200

    added option with enumerate

--- Beetroot/lesson_6/task_1_asterisk.py
+++ Beetroot/lesson_6/task_1_asterisk.py
@@ -2,9 +2,11 @@
 Сделайте словарь дней недели {1: "Monday", 2:... } в общем словарь. И потом "переверните" чтоб было {"Monday": 1, ...
 """
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# option 1
 days_of_week = {i: day for i, day in zip(range(1, 8), days)}
 print(days_of_week)
 
+# option 2
 week_days = {i: day for i, day in enumerate(days, start=1)}
 print(week_days)
 
