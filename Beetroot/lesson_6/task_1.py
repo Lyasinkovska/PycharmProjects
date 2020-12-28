ccd39ddd756c026c2a7e675de6b0c7bd455c88ff
Lyasinkovska <l.yasinkovska@gmail.com>
2020-12-28 10:36:17 +0200

    task_1 add unique words from input and count

--- Beetroot/lesson_6/task_1.py
+++ Beetroot/lesson_6/task_1.py
@@ -1,9 +1,12 @@
 """
-Сделайте словарь дней недели {1: "Monday", 2:... } в общем словарь. И потом "переверните" чтоб было {"Monday": 1, ...
+Make a program that has some sentence (a string) on input and returns a dict containing all unique words as keys
+and the number of occurrences as values.
 """
-days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-days_of_week = {i: day for i, day in zip(range(1, 8), days)}
-print(days_of_week)
-
-reversed_dict = {value: key for key, value in days_of_week.items()}
-print(reversed_dict)
+unique_words = {}
+user_input = input("Enter a sentence: ").strip().split()
+for word in user_input:
+	if word not in unique_words:
+		unique_words[word] = 1
+	else:
+		unique_words[word] += 1
+print(unique_words)
