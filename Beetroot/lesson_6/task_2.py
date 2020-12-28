cb0fcc6bcb70fd0b2c2004ea31540846b5e8765d
Lyasinkovska <l.yasinkovska@gmail.com>
2020-12-28 10:48:20 +0200

    total_price function added

--- Beetroot/lesson_6/task_2.py
+++ Beetroot/lesson_6/task_2.py
@@ -15,3 +15,28 @@ prices = {
 Create a function which takes as input two dicts with structure mentioned above,
 then computes and returns the total price of stock.
 """
+
+
+def total_price(stock, prices):
+	total_price = 0
+	for elem in stock:
+		if elem in prices:
+			total_price += stock[elem] * prices[elem]
+	return total_price
+
+
+stock = {
+	"banana": 6,
+	"apple": 0,
+	"orange": 32,
+	"pear": 15
+}
+prices = {
+	"banana": 4,
+	"apple": 2,
+	"orange": 1.5,
+	"pear": 3
+}
+
+tot_price = total_price(stock, prices)
+print(tot_price)
