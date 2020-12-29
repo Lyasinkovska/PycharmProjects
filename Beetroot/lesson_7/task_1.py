86b5a1ae575bfca5bcfbb99697301df573003601
Lyasinkovska <l.yasinkovska@gmail.com>
2020-12-29 15:24:48 +0200

    function favourite_movie

---
+++ Beetroot/lesson_7/task_1.py
@@ -0,0 +1,13 @@
+"""
+A simple function.
+
+Create a simple function called favorite_movie, which takes a string containing the name of your favorite movie.
+ The function should then print “My favorite movie is named {name}”.
+"""
+
+
+def favourite_movie(movie_name):
+	print(f'My favorite movie is named "{movie_name}"')
+
+
+favourite_movie("The age of Adaline")
