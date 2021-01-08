4b60409a659563c531918c47d2fd3212c75144b2
Lyasinkovska <l.yasinkovska@gmail.com>
2021-01-08 12:00:07 +0200

    delete draft

--- Beetroot/lesson_9/drf.py
+++
@@ -1,7 +0,0 @@
-search_actions = {'sn': ['firstname', None], 'sl': ['lastname', None],
-                  'sf': ['fullname', None], 'snm': ['number', None],
-                  'sc': ['city', None]}
-
-if __name__ == '__main__':
-    keys = [item[0] for item in search_actions.values()]
-    print(keys)
