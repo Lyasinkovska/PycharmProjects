0a76d19b64cac18fb1220c92acaab965135d2da1
Lyasinkovska <l.yasinkovska@gmail.com>
2021-01-12 13:26:11 +0200

    deleted @property decorator

--- Beetroot/lesson_10/task_3.py
+++ Beetroot/lesson_10/task_3.py
@@ -26,27 +26,26 @@ class TVController:
 
     def first_channel(self):
         self.position = 0
-        return self.current_channel
+        return self.current_channel()
 
     def last_channel(self):
         self.position = len(self.channels) - 1
-        return self.current_channel
+        return self.current_channel()
 
     def next_channel(self):
         if self.position != len(self.channels) - 1:
             self.position += 1
         else:
             self.position = 0
-        return self.current_channel
+        return self.current_channel()
 
     def previous_channel(self):
         if self.position != 0:
             self.position -= 1
         else:
             self.position = len(self.channels) - 1
-        return self.current_channel
+        return self.current_channel()
 
-    @property
     def current_channel(self):
         return self.channels[self.position]
 
@@ -58,14 +57,13 @@ if __name__ == '__main__':
 
     CHANNELS = ["BBC", "Discovery", "TV1000"]
     controller = TVController(CHANNELS)
-    print('current', controller.current_channel)
     print('last', controller.last_channel())
     for i in range(5):
         print('next', controller.next_channel())
     print('first', controller.first_channel())
     for i in range(5):
         print('previous', controller.previous_channel())
-    print('current', controller.current_channel)
+    print('current', controller.current_channel())
     print(controller.is_exist('BBC'))
     print(controller.is_exist('BBS'))
     print(controller.is_exist(1))
