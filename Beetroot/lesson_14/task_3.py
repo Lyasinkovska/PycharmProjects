fcda3c3e600b72d57fae56d0ade02efd7869b62f
Lyasinkovska <l.yasinkovska@gmail.com>
2021-01-20 15:50:25 +0200

    decorator arg_rules

---
+++ Beetroot/lesson_14/task_3.py
@@ -0,0 +1,45 @@
+"""
+Write a decorator `arg_rules` that validates arguments passed to the function.
+A decorator should take 3 arguments:
+
+max_length: 15
+type_: str
+contains: [] - list of symbols that an argument should contain
+If some of the rules' checks returns False, the function should return False and print the reason it failed; otherwise,
+return the result.
+
+"""
+import functools
+
+
+def arg_rules(_func=None, *, type_: type = str, max_length: int = 25, contains: list):
+    def decorator_arg_rules(func):
+        @functools.wraps(func)
+        def wrapper_arg_rules(*args, **kwargs):
+            string_to_check = repr(args[0])
+            check_result = type(string_to_check) == type_ and len(string_to_check) <= max_length and all(
+                elem in string_to_check for elem in contains)
+            if check_result:
+                return func(*args, **kwargs)
+            else:
+                return check_result
+
+        return wrapper_arg_rules
+
+    if _func is None:
+        return decorator_arg_rules
+    else:
+        return decorator_arg_rules(_func)
+
+
+@arg_rules(type_=str, max_length=15, contains=['05', '@'])
+def create_slogan(name: str) -> str:
+    return f"{name} drinks pepsi in his brand new BMW!"
+
+
+if __name__ == '__main__':
+    print(create_slogan('johndoe05@gmail.com'))
+    print(create_slogan('S@SH05'))
+
+    assert create_slogan('johndoe05@gmail.com') is False
+    assert create_slogan('S@SH05') == 'S@SH05 drinks pepsi in his brand new BMW!'
