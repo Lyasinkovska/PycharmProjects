dce5bba88369dc2a8d9bcc0ee452ae7a4562b0cf
Lyasinkovska <l.yasinkovska@gmail.com>
2021-01-26 11:38:51 +0200

    InRange class for calculation range of numbers

---
+++ Beetroot/lesson_16/task_2.py
@@ -0,0 +1,54 @@
+"""
+Create your own implementation of a built-in function range, named in_range(), which takes three parameters:
+`start`, `end`, and optional step. Tips: See the documentation for `range` function
+"""
+
+
+class InRange:
+    def __init__(self, *args: int):
+        self.arguments = args
+        self.start = 0
+        self.step = 1
+        self.stop = None
+        self.check_len_arguments()
+
+    def check_len_arguments(self):
+        if len(self.arguments) == 1 and self.arguments[0] > 0:
+            self.stop = self.arguments[0]
+        elif len(self.arguments) == 2 and self.arguments[0] < self.arguments[1]:
+            self.start = self.arguments[0]
+            self.stop = self.arguments[1]
+        elif len(self.arguments) == 3 and (self.arguments[0] < self.arguments[1] and self.arguments[2] > 0) \
+            or (len(self.arguments) == 3 and self.arguments[0] > self.arguments[1] and self.arguments[2] < 0):
+            self.start = self.arguments[0]
+            self.stop = self.arguments[1]
+            self.step = self.arguments[2]
+        elif self.step == 0:
+            raise Exception('Step cannot be 0')
+        else:
+            raise Exception('Wrong arguments')
+
+    def __next__(self):
+        result = self.__iter__()
+        for element in result:
+            return element
+        raise StopIteration
+
+    def __iter__(self):
+        self.start -= self.step
+        self.stop -= self.step
+        if self.start < self.stop:
+            while self.start < self.stop:
+                self.start += self.step
+                yield self.start
+        else:
+            while self.start > self.stop:
+                self.start += self.step
+                yield self.start
+
+
+if __name__ == '__main__':
+    my = InRange(5, 21, 2)
+    print(my.start, my.stop, my.step)
+    for i in my:
+        print(i)
