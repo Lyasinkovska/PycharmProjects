7ec2f37262e142052cf5de6fc5b52e2f6f2158a4
Lyasinkovska <l.yasinkovska@gmail.com>
2021-02-18 12:31:09 +0200

    right solution for horse_chess.py

--- Beetroot/lesson_23/horse_chess.py
+++ Beetroot/lesson_23/horse_chess.py
@@ -1,20 +1,21 @@
+from collections import deque
+
+
 class Horse:
 
-    def __init__(self, x: int = 0, y: int = 0):
-        self.__x = x
-        self.__y = y
-        self.dist = 0
-        self.visited_cells = [self.current_position]
+    def __init__(self, x: int = 0, y: int = 0, dist: int = 0):
+        self.x = x
+        self.y = y
+        self.dist = dist
+        self.visited_cells = []
         self.possible_steps = [(2, 1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (2, -1)]
 
     @property
     def current_position(self):
-        return self.__x, self.__y
+        return self.x, self.y
 
-    @current_position.setter
-    def current_position(self, new_position):
-        self.__x = new_position[0]
-        self.__y = new_position[1]
+    def __repr__(self):
+        return f'({self.x}, {self.y}): {self.dist}'
 
 
 class Board:
@@ -26,20 +27,24 @@ class Board:
         return 1 <= position[0] <= self.size and 1 <= position[1] <= self.size
 
     def shortest_way(self, figure, destination):
+        queue = deque()
+        queue.append(figure)
 
-        for step in figure.possible_steps:
-            if figure.current_position == destination:
-                return figure.dist, figure.visited_cells
+        while queue:
 
-            next_position = figure.current_position[0] + step[0], figure.current_position[1] + step[1]
+            current = queue[0]
+            print(queue)
+            queue.popleft()
 
-            if self.is_valid(next_position) and self.is_valid(destination) and next_position not in figure.visited_cells:
-                # print(figure.current_position)
-                figure.current_position = next_position
-                figure.dist += 1
-                # print('next', next_position)
-                figure.visited_cells.append(next_position)
-                self.shortest_way(figure, destination)
+            if current.current_position == destination:
+                return current.dist
+
+            for step in figure.possible_steps:
+                next_position = current.x + step[0], current.y + step[1]
+
+                if self.is_valid(next_position) and self.is_valid(destination) and next_position not in figure.visited_cells:
+                    figure.visited_cells.append(next_position)
+                    queue.append(Horse(next_position[0], next_position[1], current.dist + 1))
 
 
 if __name__ == '__main__':
