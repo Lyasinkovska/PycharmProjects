2835f6fbdffab55ee91e27e4976eea894b599810
Lyasinkovska <l.yasinkovska@gmail.com>
2021-02-22 15:52:10 +0200

    binary_search

--- Beetroot/lesson_25/task_1.py
+++ Beetroot/lesson_25/task_1.py
@@ -21,7 +21,5 @@ if __name__ == '__main__':
     collection = [i for i in range(100)]
     print(binary_search(99, collection))
     print(binary_search(120, collection))
-    animals = ['cat', 'dog', 'bird', 'snake', 'elephant', 'tiger', 'cow']
-    print(binary_search('tiger', animals))
-    print(binary_search('lion', animals))
+
 
