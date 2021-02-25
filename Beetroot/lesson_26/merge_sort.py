2829db94a6f47e3714c869340276875afb1b5a90
Lyasinkovska <l.yasinkovska@gmail.com>
2021-02-25 11:54:19 +0200

    merge sort

---
+++ Beetroot/lesson_26/merge_sort.py
@@ -0,0 +1,34 @@
+"""
+Implement the mergeSort function without using the slice operator.
+"""
+
+
+def merge_sort(array, left_index=0, right_index=None):
+    if right_index is None:
+        right_index = len(array) - 1
+
+    if right_index - left_index > 1:
+
+        middle = (left_index + right_index) // 2
+        merge_sort(array, left_index, middle)
+        merge_sort(array, middle, right_index)
+
+        merged = []
+        i = left_index
+        j = middle
+        while i < middle and j < right_index:
+            if array[i] <= array[j]:
+                merged.append(array[i])
+                i = i + 1
+            else:
+                merged.append(array[j])
+                j = j + 1
+
+        merged.extend(array[i:middle])
+        array[left_index:left_index + len(merged)] = merged
+    return array
+
+
+if __name__ == '__main__':
+    array = [19, 1, 50, 21, 2, 23]  # [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print(merge_sort(array))
